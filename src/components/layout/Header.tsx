7d5e5ed855a58745e08806704a6042ad88028ece
Janitz_De_Paz <janitzdp@gmail.com>
2026-03-22 22:04:35 +0100

    feat: update Header component styles and improve list item appearance

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,13 @@
 import { headerItems } from "../../constants/headerItems";
 
 export const Header = () => (
-  <header className="fixed top-0 h-[10vh] w-screen bg-gray-800 flex justify-center">
+  <header className="fixed inset-0 h-(--header-height) bg-(--header-bg) flex justify-center border-b border-(--primary-border) z-50">
     <ul className="flex gap-5 justify-center items-center">
       {headerItems.map((item) => (
-        <li key={item.id} className="text-white">
+        <li
+          key={item.id}
+          className="text-(--text-muted) hover:text-(--text-main)"
+        >
           <a href={item.link}>{item.name}</a>
         </li>
       ))}
