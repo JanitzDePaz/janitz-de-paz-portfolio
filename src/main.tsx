cdcc692a01553fa65013d89105dcbfa1d74c5013
Janitz_De_Paz <janitzdp@gmail.com>
2026-03-30 23:57:01 +0200

    refactor(main): update project structure and remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { Header } from "./components/layout/Header";
-import { AboutMe } from "./components/features/AboutMe";
+import { Header } from "./components/layout/header/Header";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <>
-      <Header />
-      <main className="flex flex-col bg-(--bg-color)">
-        <AboutMe />
-      </main>
-    </>
-  </StrictMode>,
+  <>
+    <Header />
+    <main className="flex flex-col bg-(--bg-color)"></main>
+  </>,
 );
