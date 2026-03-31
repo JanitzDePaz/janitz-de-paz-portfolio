e7a9fbde498b731a88c37e11bff70ab3efda3090
Janitz_De_Paz <janitzdp@gmail.com>
2026-03-31 22:19:29 +0200

    feat(store): add mobileWidth and setMenu to header state

--- src/stores/headerStore.ts
+++ src/stores/headerStore.ts
@@ -3,6 +3,8 @@ import { create } from "zustand";
 export const headerMenu = create<HeaderMenu>((set) => ({
   openMenu: false,
   width: 0,
+  mobileWidth: 768,
   toggleMenu: () => set((state) => ({ openMenu: !state.openMenu })),
+  setMenu: (value) => set({openMenu: value}),
   setWidth: (width) => set({ width: width }),
 }));
--- src/types/headerTypes.d.ts
+++ src/types/headerTypes.d.ts
@@ -1,6 +1,8 @@
 type HeaderMenu = {
   openMenu: boolean;
   width: number;
+  mobileWidth: number;
   toggleMenu: () => void;
+  setMenu: (menu: boolean) => void;
   setWidth: (width: number) => void;
 };
