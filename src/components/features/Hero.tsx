d1d8210d7c7560daefde62861a82e80c62c1f859
Janitz_De_Paz <janitzdp@gmail.com>
2026-04-01 23:18:37 +0200

    feat(hero): implement mobile-first structure with Montserrat and Roboto

---
+++ src/components/features/Hero.tsx
@@ -0,0 +1,22 @@
+import arrowDown from "../../assets/icons/arrowDown.svg";
+export const Hero = () => {
+  return (
+    <section className="flex justify-center items-center text-center">
+      <div className="flex flex-col gap-5 justify-center items-center">
+        <h1 className="text-5xl title">Janitz De Paz</h1>
+        <h2 className="text-3xl text-(--accent)  subtitle">
+          React & TypeScript <br /> developer
+        </h2>
+        <a
+          href="#SobreMi"
+          className="primary-btn transition-colors"
+        >
+          <button>Conoceme</button>
+        </a>
+        <a href="#SobreMi">
+          <img src={arrowDown} alt="Flecha direccional" />
+        </a>
+      </div>
+    </section>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Header } from "./components/layout/header/Header";
+import { Hero } from "./components/features/Hero";
 
 createRoot(document.getElementById("root")!).render(
   <>
     <Header />
-    <main className="flex flex-col bg-(--bg-color)"></main>
+    <main className="flex flex-col bg-(--bg-color) pt-(--header-height)">
+      <Hero />
+    </main>
   </>,
 );
