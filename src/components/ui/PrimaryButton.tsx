f5ce16b2ff16c3f593758ba355bda4c5485aa296
Janitz_De_Paz <janitzdp@gmail.com>
2026-04-03 00:31:16 +0200

    style(ui): add hover and active states to PrimaryButton

--- src/components/ui/PrimaryButton.tsx
+++ src/components/ui/PrimaryButton.tsx
@@ -3,7 +3,7 @@ export const PrimaryButton = ({
   children,
   className = "",
 }: PrimaryButtonType) => {
-  const buttonStyle = `font-(--font-body) font-semibold text-lg bg-white text-black px-16 py-2 rounded-full transition-transform duration-200 ease ${className}`;
+  const buttonStyle = `font-(--font-body) font-semibold text-lg bg-white text-black px-16 py-2 rounded-full transition-all duration-200 ease hover:bg-[#cacaca] hover:scale-[1.03] active:scale-95 ${className}`;
 
   if (href == null || href == "") {
     return <button className={buttonStyle}>{children}</button>;
