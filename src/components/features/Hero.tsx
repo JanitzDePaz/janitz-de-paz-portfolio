1b3c30b10ab63700d48543af1882e6cc6520b3cf
Janitz_De_Paz <janitzdp@gmail.com>
2026-04-05 03:01:59 +0200

    feat(hero): implement animations in Hero section

--- src/components/features/Hero.tsx
+++ src/components/features/Hero.tsx
@@ -1,18 +1,39 @@
+import { useEffect } from "react";
 import arrowDown from "../../assets/icons/arrowDown.svg";
 import { PrimaryButton } from "../ui/PrimaryButton";
 import { Spotlight } from "../ui/Spotlight";
+import { scrollAnimation } from "../../animations/scrollAnimation";
+import { gsap } from "gsap";
 export const Hero = () => {
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      scrollAnimation({
+        className: ".anim",
+        direction: "up",
+        distance: 30,
+        delay: 0.2,
+        start: "top 85%",
+        duration: 1,
+        stagger: 0.2,
+      });
+    });
+
+    return () => ctx.revert();
+  }, []);
+
   return (
     <section className="flex justify-center items-center text-center overflow-hidden relative">
-      <div className="flex flex-col gap-5 justify-center items-center">
-        <h1 className="text-3xl sm:text-5xl xl:text-6xl title">
+      <div className="flex flex-col gap-8 justify-center items-center">
+        <h1 className="text-3xl sm:text-5xl xl:text-6xl title anim">
           Janitz De Paz
         </h1>
-        <h2 className="text-xl sm:text-3xl xl:text-4xl text-(--accent) subtitle">
+        <h2 className="text-xl sm:text-3xl xl:text-4xl text-(--accent) subtitle anim">
           React & TypeScript <br /> developer
         </h2>
-        <PrimaryButton href="#SobreMi">Conóceme</PrimaryButton>
-        <a href="#SobreMi">
+        <div className="anim">
+          <PrimaryButton href="#SobreMi">Conóceme</PrimaryButton>
+        </div>
+        <a href="#SobreMi" className="anim animate-bounce">
           <img src={arrowDown} alt="Flecha direccional" />
         </a>
       </div>
