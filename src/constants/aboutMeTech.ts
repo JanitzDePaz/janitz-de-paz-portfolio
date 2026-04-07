31e65d0b1d79b11a58298f0743df07c93847d53a
Janitz_De_Paz <janitzdp@gmail.com>
2026-04-07 02:41:22 +0200

    feat(assets): add tech icons and aboutMeTech constants

---
+++ src/constants/aboutMeTech.ts
@@ -0,0 +1,12 @@
+import ReactIcon from "../assets/icons/reactIcon.svg";
+import TypeScriptIcon from "../assets/icons/typeScriptIcon.svg";
+import GitHubIcon from "../assets/icons/githubIcon.svg";
+import ViteIcon from "../assets/icons/viteIcon.svg";
+import TailwindIcon from "../assets/icons/tailwindIcon.svg";
+export const aboutMeTech = [
+  { name: "React", alt: "React icon", url: ReactIcon },
+  { name: "TypeScript", alt: "TypeScript icon", url: TypeScriptIcon },
+  { name: "Github", alt: "GitHub Icon", url: GitHubIcon },
+  { name: "Vite", alt: "Vite Icon", url: ViteIcon },
+  { name: "Tailwind CSS", alt: "Tailwind CSS icon", url: TailwindIcon },
+];
