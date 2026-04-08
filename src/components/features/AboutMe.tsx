0af9b966c3401f03b7bee28ddba600193a9a9a04
Janitz_De_Paz <janitzdp@gmail.com>
2026-04-09 00:35:33 +0200

    feat(AboutMe): implement responsive desktop layout

--- src/components/features/AboutMe.tsx
+++ src/components/features/AboutMe.tsx
@@ -4,31 +4,42 @@ import { Title } from "../ui/Title";
 import arrowDown from "../../assets/icons/arrowDown.svg";
 
 export const AboutMe = () => (
-  <section className="text-center aboutMeBg" id="SobreMi">
-    <div className="flex flex-col justify-center items-center gap-8 w-4/5">
-      <div>
+  <section className="aboutMeBg" id="SobreMi">
+    <div className="flex flex-col lg:flex-row justify-center items-center py-10 gap-15 lg:gap-20">
+      <div className="text-center lg:text-left flex flex-col gap-6 w-2/3 lg:w-4/12">
         <Title>Sobre mi</Title>
-        <p className="text-lg w-4/5">
+        <p className="text-lg ">
           Estudiante de 1º de DAW apasionado por el desarrollo web Full-stack.
           Enfocado en construir soluciones escalables con React y TypeScript,
           priorizando siempre la calidad del código y la arquitectura limpia.
         </p>
-        <div className="p-10 grid grid-cols-3 grid-rows-2 gap-5">
-          {aboutMeTech.map((icon) => (
-            <img
-              key={icon.name}
-              alt={icon.alt}
-              src={icon.url}
-              className="w-full aspect-square"
-            />
-          ))}
+        <hr className="hidden lg:block" />
+        <div className="hidden lg:flex lg:flex-col lg:gap-5 lg:justify-center lg:items-center w-fit m-auto">
+          <PrimaryButton className="" href="#SobreMi">
+            Mis proyectos
+          </PrimaryButton>
+          <a href="#SobreMi" className="animate-bounce">
+            <img src={arrowDown} alt="Flecha direccional" />
+          </a>
         </div>
       </div>
 
-      <PrimaryButton href="#SobreMi">Mis proyectos</PrimaryButton>
-      <a href="#SobreMi" className="animate-bounce">
-        <img src={arrowDown} alt="Flecha direccional" />
-      </a>
+      <div className="grid grid-cols-3 grid-rows-2 place-items-center gap-5">
+        {aboutMeTech.map((icon) => (
+          <img
+            key={icon.name}
+            alt={icon.alt}
+            src={icon.url}
+            className="w-20 lg:w-30 aspect-square"
+          />
+        ))}
+      </div>
+      <div className="lg:hidden flex flex-col gap-5 justify-center items-center w-fit">
+        <PrimaryButton href="#SobreMi">Mis proyectos</PrimaryButton>
+        <a href="#SobreMi" className="animate-bounce">
+          <img src={arrowDown} alt="Flecha direccional" />
+        </a>
+      </div>
     </div>
   </section>
 );
