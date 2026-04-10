dd01ce9e7a354c19e0b64c84f42b027a379ed7b4
Janitz_De_Paz <janitzdp@gmail.com>
2026-04-10 02:16:01 +0200

    refactor(animation): improve scrollToId duration and add autoKill

--- src/animations/scrollToId.ts
+++ src/animations/scrollToId.ts
@@ -1,12 +1,12 @@
-import gsap from "gsap"
+import gsap from "gsap";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 gsap.registerPlugin(ScrollToPlugin);
 
-
 export const scrollToId = (url: string) => {
-    gsap.to(window, {
-        duration: 0.5,
-        scrollTo: { y: url},
-        ease: "power2.inOut"
-    })
-}
+  gsap.to(window, {
+    duration: 0.8,
+    scrollTo: { y: url },
+    autoKill: true,
+    ease: "power2.inOut",
+  });
+};
