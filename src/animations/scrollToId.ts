b89fea37dce7fa0c975465f09227ba5b5cab3942
Janitz_De_Paz <janitzdp@gmail.com>
2026-04-10 19:07:19 +0200

    fix(scrollToId): move autokill to scrollTo object and add offset for better UX

--- src/animations/scrollToId.ts
+++ src/animations/scrollToId.ts
@@ -3,10 +3,13 @@ import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 gsap.registerPlugin(ScrollToPlugin);
 
 export const scrollToId = (url: string) => {
+
+  const offset = window.innerHeight * 0.07;
+
   gsap.to(window, {
     duration: 0.8,
-    scrollTo: { y: url },
-    autoKill: true,
+    scrollTo: { y: url, autoKill: true, offsetY: offset },
+   
     ease: "power2.inOut",
   });
 };
