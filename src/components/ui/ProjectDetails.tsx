b448d8c31ac7876644ce3ae6e8bd7b048c21dc52
Janitz <janitzdp@gmail.com>
2026-04-20 13:22:37 +0200

    feat(ProjectDetails): enhance layout and add ActionButton for project links

--- src/components/ui/ProjectDetails.tsx
+++ src/components/ui/ProjectDetails.tsx
@@ -2,6 +2,7 @@ import { projectsData } from "../../constants/projectsData";
 import { useProjectStore } from "../../stores/projectStore";
 import githubIcon from "../../assets/icons/githubIcon.svg";
 import webIcon from "../../assets/icons/webIcon.svg";
+import { ActionButton } from "./ActionButton";
 
 export const ProjectDetails = ({ projectId }: ProjectId) => {
   const project = projectsData.find((proj) => proj.projectId === projectId);
@@ -10,9 +11,9 @@ export const ProjectDetails = ({ projectId }: ProjectId) => {
   console.log(project);
 
   return !project || !moreInfo ? null : (
-    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 min-w-[80vw] h-[80vh] bg-[#100f0ffa] rounded-4xl border border-white text-white flex justify-center items-center p-10">
-      <div className="h-full relative grid grid-cols-3 gap-15 p-10">
-        <div className="flex flex-col gap-5">
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 min-w-[80vw] h-[80vh] bg-[#100f0ffc] rounded-4xl border border-white text-white flex justify-center items-center p-20">
+      <div className="h-full relative flex justify-center items-center gap-20 flex-wrap">
+        <div className="flex flex-col w-4/12 h-9/12">
           {project.mediaType === "image" ? (
             <img
               src={project.media}
@@ -22,47 +23,66 @@ export const ProjectDetails = ({ projectId }: ProjectId) => {
           ) : (
             <video src={project.media} className="aspect-video" />
           )}
-          <h1 className="text-4xl">{project.name}</h1>
-          <div className="flex w-full flex-1 justify-center items-center">
-            <a href={project.GitHub.url} className="w-30">
-              <img src={githubIcon} alt="GitHub icon" />
-            </a>
+
+          <div className="flex flex-wrap justify-center items-center gap-5 py-10">
+            <ActionButton
+              href={project.GitHub.url}
+              className="flex justify-center items-center gap-5 py-6 px-10 h-15 w-50 border border-gray-700 bg-gray-800/50 rounded-xl"
+            >
+              <img src={githubIcon} alt="GitHub icon" className="w-10" />
+              <p>GitHub</p>
+            </ActionButton>
             {project.webPage == null ? null : (
-              <a href={project.webPage} className="w-1/3">
-                <img src={webIcon} alt="WebPage icon" className="w-full" />
-              </a>
+              <ActionButton
+                href={project.webPage}
+                className="flex justify-center items-center gap-5 py-6 px-10 h-15 w-50 border border-gray-700 bg-gray-800/50 rounded-xl"
+              >
+                <img src={webIcon} alt="WebPage icon" className="w-10" />
+                <p>Web</p>
+              </ActionButton>
             )}
           </div>
         </div>
-        <div className="col-span-2 h-8/9 mx-20 flex flex-col gap-10 overflow-y-auto">
-          <div className="flex flex-col gap-3">
-            <h2 className="text-3xl">Descripcion</h2>
-            <p>{project.description}</p>
-          </div>
-          <div className="flex flex-col gap-3">
-            <h2 className="text-3xl">Aprendizaje</h2>
-            <p>{project.learning}</p>
-          </div>
-          <div className="flex flex-col gap-3">
-            <h2 className="text-3xl">Retos</h2>
-            <ol className="flex flex-col gap-2 list-disc mx-10">
-              {project.challenges.map((challenge, i) => (
-                <li key={i}>{challenge}</li>
-              ))}
-            </ol>
+
+        <div className="w-6/12 flex flex-col gap-10 mx-10 h-9/12">
+          <h1 className="text-5xl font-bold">{project.name}</h1>
+          <div className="flex flex-col gap-7 overflow-y-scroll ">
+            <div className="flex flex-col gap-3">
+              <h2 className="text-3xl">Descripcion</h2>
+              <p>{project.description}</p>
+            </div>
+            <div className="flex flex-col gap-3">
+              <h2 className="text-3xl">Aprendizaje</h2>
+              <p>{project.learning}</p>
+            </div>
+            <div className="flex flex-col gap-3">
+              <h2 className="text-3xl">Retos</h2>
+              <ol className="flex flex-col gap-2 list-disc mx-10">
+                {project.challenges.map((challenge, i) => (
+                  <li key={i}>{challenge}</li>
+                ))}
+              </ol>
+            </div>
           </div>
         </div>
 
-        <div className="flex gap-5 text-center col-span-3 mx-auto">
+        <div className="flex flex-wrap gap-6 justify-center items-center self-center w-full">
           {project.tech.map((tech, i) => (
-            <div key={i} className="flex flex-col">
-              <img src={tech.img} alt={`${tech.name} icon`} className="h-20 w-20 bg-gray-400 rounded-xl aspect-square object-contain" />
+            <div
+              key={i}
+              className="h-25 px-4 py-3 rounded-2xl flex flex-col gap-2 items-center justify-center border border-gray-700 bg-gray-800/50 shadow-xl"
+            >
+              <img
+                src={tech.img}
+                alt={`${tech.name} icon`}
+                className="h-full w-auto max-h-20 max-w-26 object-contain"
+              />
               <h3>{tech.name}</h3>
             </div>
           ))}
         </div>
         <button
-          className="border absolute top-0 right-0 rounded-4xl aspect-square w-15"
+          className="border absolute -top-10 -right-10 rounded-lg aspect-square w-15"
           onClick={() => setMoreInfo(false)}
         >
           X
