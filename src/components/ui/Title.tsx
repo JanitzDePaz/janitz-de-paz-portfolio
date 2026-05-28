159f774d83d490802623f1401b2c2ad09ae9cd7b
Janitz <janitzdp@gmail.com>
2026-05-28 21:47:19 +0200

    fix(Title): remove font-bold and tracking-tight classes for cleaner styling

--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -1,7 +1,5 @@
 export const Title = ({ children, className = "" }: TitlesTypes) => (
-  <h1
-    className={`text-3xl sm:text-5xl xl:text-6xl title font-bold tracking-tight ${className}`}
-  >
+  <h1 className={`text-3xl sm:text-5xl xl:text-6xl title ${className}`}>
     {children}
   </h1>
 );
