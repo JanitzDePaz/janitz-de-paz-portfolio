861e74212c9a709d938f48fffc9ee2bde0daae66
Janitz <janitzdp@gmail.com>
2026-09-13 13:26:48 +0200

    feat(Projects): add new project "Tienda de videojuegos" with updated description and media

--- src/constants/projectsData.ts
+++ src/constants/projectsData.ts
@@ -17,7 +17,7 @@ export const projectsData = [
     media: "/images/plug-in-preview.png",
     mediaType: "image",
     description:
-      "Este es mi proyecto más grande hasta ahora, una tienda de periféricos. Tuve la idea de hacer un ecommerce por su escalabilidad y he usado este proyecto para probar tecnologías del mundo del desarrollo web",
+      "Este es mi proyecto más grande hasta ahora, una tienda de periféricos. Tuve la idea de hacer un ecommerce por su escalabilidad y he usado este proyecto para probar tecnologías del mundo del desarrollo web.",
     GitHub: {
       text: "Github",
       url: "https://github.com/JanitzDePaz/plug-in-ecommerce",
@@ -29,11 +29,11 @@ export const projectsData = [
   },
   {
     projectId: 3,
-    name: "Próximamente",
-    media: "/images/next-project.png",
+    name: "Tienda de videojuegos",
+    media: "/images/tiendaVideojuegos.png",
     mediaType: "image",
     description:
-      "Espacio reservado para el siguiente gran proyecto. Actualmente en desarrollo...",
+      "Proyecto en grupo creado durante mis practicas en Ayesa. Es una tienda de videojuegos donde el usuario puede registrarse, iniciar sesión y simular la compra de videojuegos. Utilizamos Thymeleaf, Spring Boot y H2.",
     GitHub: {
       text: "LinkedIn",
       url: "https://linkedin.com/in/janitz-de-paz",
